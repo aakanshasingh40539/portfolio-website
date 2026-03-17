a4d7b0bd4ce90e7c61d9b39e53bb5326ac0f7ecc
aakanshasingh40539 <aakanshasingh40539@gmail.com>
2026-03-17 15:54:26 +0530

    Add files via upload

---
+++ todo-app/script.js
@@ -0,0 +1,25 @@
+function addTask(){
+
+let input=document.getElementById("taskInput");
+let task=input.value;
+
+if(task===""){
+alert("Enter a task");
+return;
+}
+
+let li=document.createElement("li");
+
+li.innerHTML=`
+${task}
+<button onclick="deleteTask(this)">X</button>
+`;
+
+document.getElementById("taskList").appendChild(li);
+
+input.value="";
+}
+
+function deleteTask(button){
+button.parentElement.remove();
+}
